3c04264c99da8fc603897169df2acd685de965bd
Pana <panayot.atanasov@gmail.com>
2021-03-30 18:11:25 +0300

    Security Update

--- src/main/java/com/example/demosecurity/model/Student.java
+++ src/main/java/com/example/demosecurity/model/Student.java
@@ -1,14 +1,25 @@
 package com.example.demosecurity.model;
 
 public class Student {
-    private Long id;
+    private Integer id;
     private String firstName;
     private String lastName;
 
-    public Student(String firstName, String lastName) {
+    public Student(Integer id,String firstName, String lastName) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    public Integer getId() {
+        return this.id;
+    }
 
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
 }
